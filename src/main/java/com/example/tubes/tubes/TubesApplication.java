1b41301aaf173963f4b79ef44ed5c4c2be08cad1
gori <rizki.hidayatulloh@mhs.itenas.ac.id>
2025-05-27 09:41:17 +0700

    pertama

---
+++ src/main/java/com/example/tubes/tubes/TubesApplication.java
@@ -0,0 +1,13 @@
+package com.example.tubes.tubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesApplication.class, args);
+	}
+
+}
